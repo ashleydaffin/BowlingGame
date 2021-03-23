1e61210dc3c35d6ed8560b535b7f6fbc9ba9aa2a
Ashley Daffin <ashleyedaffin@gmail.com>
2021-03-22 18:22:10 -0700

    Add score variable to BowlingGame update methods to include new variable

--- src/main/java/BowlingGame.java
+++ src/main/java/BowlingGame.java
@@ -1,9 +1,11 @@
 public class BowlingGame {
 
-    public void roll(int pins){
+    private int score = 0;
 
+    public void roll(int pins){
+    score += pins;
     }
     public int score(){
-        return 0;
+        return score;
     }
 }
