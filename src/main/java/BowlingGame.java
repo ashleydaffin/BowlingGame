e54140bb6046f69d42bd7c309444533073e8167a
Ashley Daffin <ashleyedaffin@gmail.com>
2021-03-23 00:14:55 -0700

    create frames to count score two rolls at a time and add if statement for spares

--- src/main/java/BowlingGame.java
+++ src/main/java/BowlingGame.java
@@ -7,8 +7,16 @@ public class BowlingGame {
     }
     public int score(){
         int score=0;
-        for(int i=0; i<rolls.length; i++)
-            score +=rolls[i];
+        int i =0;
+        for(int frame=0; frame<10; frame++){
+            if (rolls[i] + rolls[i+1] == 10){ //spare
+                score += 10 + rolls[i+2];
+                i+=2;
+            } else {
+                score += rolls[i] + rolls[i + 1];
+                i += 2;
+            }
+        }
         return score;
     }
 }
