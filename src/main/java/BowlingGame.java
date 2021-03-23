0a70ce328551b079b41c5cf242af46eef7051e5e
Ashley Daffin <ashleyedaffin@gmail.com>
2021-03-22 17:42:15 -0700

    Create test class and BowlingGame class to practice bowling game kata with JUnit5

---
+++ src/main/java/BowlingGame.java
@@ -0,0 +1,2 @@
+public class BowlingGame {
+}
---
+++ src/test/java/BowlingGameTest.java
@@ -0,0 +1,13 @@
+
+import org.junit.jupiter.api.Disabled;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+public class BowlingGameTest {
+
+    @Test
+    @DisplayName("Test for zero score gutter game")
+    public void testForGutterGameScoreIsZero(){
+        Game g = new Game();
+    }
+}
