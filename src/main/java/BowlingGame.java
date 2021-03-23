3d816588252ca9fa4c05545a086849dc6a6abfb5
Ashley Daffin <ashleyedaffin@gmail.com>
2021-03-22 17:45:27 -0700

    Set up first test: gutter game should score zero

--- src/main/java/BowlingGame.java
+++ src/main/java/BowlingGame.java
@@ -1,2 +1,6 @@
 public class BowlingGame {
+
+    public void roll(int pins){
+
+    }
 }
--- src/test/java/BowlingGameTest.java
+++ src/test/java/BowlingGameTest.java
@@ -1,5 +1,4 @@
 
-import org.junit.jupiter.api.Disabled;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
@@ -8,6 +7,8 @@ public class BowlingGameTest {
     @Test
     @DisplayName("Test for zero score gutter game")
     public void testForGutterGameScoreIsZero(){
-        Game g = new Game();
+        BowlingGame game = new BowlingGame();
+        for (int i=0; i<20; i++)
+            game.roll(0);
     }
 }
