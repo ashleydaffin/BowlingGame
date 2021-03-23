69e136455dc6851dceda6257de64a9fdf78f2a07
Ashley Daffin <ashleyedaffin@gmail.com>
2021-03-22 23:51:08 -0700

    Add @BeforeEach prior to setUp() method, otherwise test will fail as method is never called within test

--- src/test/java/BowlingGameTest.java
+++ src/test/java/BowlingGameTest.java
@@ -1,4 +1,5 @@
 
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
@@ -7,23 +8,27 @@ import static org.junit.jupiter.api.Assertions.assertEquals;
 public class BowlingGameTest {
     private BowlingGame game;
 
-    protected void setUp() throws Exception{
+    @BeforeEach
+   protected void setUp() {
         game = new BowlingGame();
+    }
+
+    public void rollMany(int n, int pins){
+        for (int i=0; i<n; i++)
+            game.roll(pins);
     }
 
     @Test
     @DisplayName("Test for zero score gutter game")
-    public void testForGutterGameScoreIsZero(){
-        for (int i=0; i<20; i++)
-            game.roll(0);
+    public void testForGutterGameScoreIsZero() {
+        rollMany(20, 0);
         assertEquals(0, game.score());
     }
 
     @Test
     @DisplayName("Test for score to equal 20, if each frame has a score of 1")
-    public void testForAllOnesScored(){
-        for(int i=0; i<20; i++)
-            game.roll(1);
+    public void testForAllOnesScored() {
+        rollMany(20,1);
         assertEquals(20, game.score());
     }
 }
