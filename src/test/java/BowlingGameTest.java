c03e890b3b4e0614b753ec5347398bb4929320d6
Ashley Daffin <ashleyedaffin@gmail.com>
2021-03-22 18:20:47 -0700

    Extract duplicated game creation

--- src/test/java/BowlingGameTest.java
+++ src/test/java/BowlingGameTest.java
@@ -5,11 +5,15 @@ import org.junit.jupiter.api.Test;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
 public class BowlingGameTest {
+    private BowlingGame game;
+
+    protected void setUp() throws Exception{
+        game = new BowlingGame();
+    }
 
     @Test
     @DisplayName("Test for zero score gutter game")
     public void testForGutterGameScoreIsZero(){
-        BowlingGame game = new BowlingGame();
         for (int i=0; i<20; i++)
             game.roll(0);
         assertEquals(0, game.score());
@@ -18,7 +22,6 @@ public class BowlingGameTest {
     @Test
     @DisplayName("Test for score to equal 20, if each frame has a score of 1")
     public void testForAllOnesScored(){
-        BowlingGame game = new BowlingGame();
         for(int i=0; i<20; i++)
             game.roll(1);
         assertEquals(20, game.score());
