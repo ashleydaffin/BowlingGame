751905ac0712ef42e4153211f950a6590258fc83
Ashley Daffin <ashleyedaffin@gmail.com>
2021-03-24 22:19:57 -0700

    Refactor and extract sumOfBallsInFrame()

--- src/main/java/BowlingGame.java
+++ src/main/java/BowlingGame.java
@@ -17,11 +17,15 @@ public class BowlingGame {
                 score += 10 + spareBonus(frameIndex);
                 frameIndex+=2;
             } else {
-                score += rolls[frameIndex] + rolls[frameIndex + 1];
+                score += sumOfBallsInFrame(frameIndex);
                 frameIndex += 2;
             }
         }
         return score;
+    }
+
+    private int sumOfBallsInFrame(int frameIndex){
+        return rolls[frameIndex] + rolls[frameIndex + 1];
     }
     private boolean isSpare(int frameIndex){
        return rolls[frameIndex] +
